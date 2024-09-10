9d4cf03c801c5e41426132c669ebe2fed4d66fa7
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-10 13:50:40 +0100

    typed annotated add function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+def add(a: float, b: float) -> float:
+    """" A function that adds two numbers passed as arg,
+    the args was typed annotated, throws err if other type is passed
+        return:
+            a + b
+    """"
+    return a + b
