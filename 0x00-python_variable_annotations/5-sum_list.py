d7927aa63ad04e08d218616dac1563e7cdb74c14
Abdulquyum <ajumobiabdulquyum@gmail.com>
2024-09-16 14:34:04 +0100

    Annotated Sum list function

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
 """ Sum up a list values """
 
+from typing import List
 
-def sum_list(input_list: list) -> float:
+
+def sum_list(input_list: List[float]) -> float:
     """Sum parameters of the list passed """
     total = 0
     for input in input_list:
